66cf7cc24165c64a6d6285aa3859a8b3617a1a89
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-07 19:00:17 +0100

    Whats New styling

--- src/hooks/useMetrics.ts
+++ src/hooks/useMetrics.ts
@@ -1,62 +1,27 @@
-import axios, {AxiosResponse} from 'axios'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-const METRICS_API_ENDPOINT = 'https://soy-finance.deta.dev/soy_metrics'
-
-const METRICS_DEFAULT_ZEROES = {
-  runtime: 0.031,
-  block_number: 8392129,
-  result: {
-    Users: 0,
-    Trades: 0,
-    Volume: 0,
-    Volume_24h: 0,
-    Soy_Circulating_Supply: 0,
-    Soy_IDO: 0,
-    Total_Value_Locked_In_Farms: 0,
-  },
-}
-
-const METRICS_DEFAULT_MOCK = {
-  runtime: 0.031,
-  block_number: 8392129,
-  result: {
-    Users: 8930,
-    Trades: 4090000,
-    Volume: 89130000,
-    Volume_24h: 152610,
-    Soy_Circulating_Supply: 73380000,
-    Soy_IDO: 798570,
-    Total_Value_Locked_In_Farms: 2340000,
-  },
-}
-
-const CONFIG_USE_MOCK_DATA = true
-const CONFIG_API_TRIES = 10
+const METRICS = 'https://soy-finance.deta.dev/soy_metrics'
 
 export const useMetrics = () => {
-  const [metricsData, setMetricsData] = useState(CONFIG_USE_MOCK_DATA ? METRICS_DEFAULT_MOCK : METRICS_DEFAULT_ZEROES)
+  const [metricsData, setMetricsData] = useState({
+    runtime: 0.031,
+    block_number: 8392129,
+    result: {
+      Users: 0,
+      Trades: 0,
+      Volume: 0,
+      Volume_24h: 0,
+      Soy_Circulating_Supply: 0,
+      Soy_IDO: 0,
+      Total_Value_Locked_In_Farms: 0,
+    },
+  })
 
   useEffect(() => {
     const fetchData = async () => {
-      for(let i = 0; i < CONFIG_API_TRIES; i++) {
-        let response: AxiosResponse<any>
-        let errorEncountered = false;
-
-        try { // eslint-disable-next-line
-          response = await axios.get(METRICS_API_ENDPOINT)
-        } catch(err) {
-          console.error(err)
-          errorEncountered = true
-        }
-
-        if(!errorEncountered && response !== undefined) {
-          setMetricsData(response.data)
-          break
-        }
-      }
-      /* axios
-        .get(METRICS_API_ENDPOINT)
+      axios
+        .get(METRICS)
         .then((response) => {
           if (response.data) {
             setMetricsData(response.data)
@@ -64,9 +29,8 @@ export const useMetrics = () => {
         })
         .catch((err) => {
           console.error(err)
-        }) */
+        })
     }
-
     fetchData()
   }, [])
 
@@ -139,3 +103,12 @@ export const useGetFarmsApr = () => {
 
   return data
 }
+
+/*
+export const useFarmsFromBlockchain = () => { // logic copied from multichain-soy-finance
+  const chainID = 820
+  const { data: farmsLP, userDataLoaded } = useFarms() // EXPAND
+
+  const activeFarms = farmsLP[chainId].filter((farm) => farm.pid !== 0 && !isArchivedPid(farm.pid))
+}
+*/
--- src/pages/Oneinch/Oneinch.tsx
+++ src/pages/Oneinch/Oneinch.tsx
@@ -4,11 +4,21 @@ import Spacer from 'components/Spacer';
 import { Theme } from 'constants/theme';
 import { Assets } from 'constants/images';
 import { useTranslation } from 'contexts/Localization';
+import { useGetBurnedSoy } from 'hooks/useMetrics';
+
+import Line from 'components/Line';
+
+import WhatsNewXT from "assets/images/WhatsNewXT.png"
+import WhatsNewStaking from "assets/images/WhatsNewStaking.png"
+import WhatsNewFarming from "assets/images/WhatsNewFarming.png"
+import WhatsNewBurn from "assets/images/WhatsNewBurn.png"
 
 const desc = `Combining a safelist for audited tokens, decentralized insurance, and the most advanced token standard, SOY Finance adopts the industry’s best practices and technologies to provide the community with a rock-solid platform.`
 
 const OneToken = () => {
     const { t } = useTranslation()
+
+    const burnedSoy = useGetBurnedSoy()
 
     return (
         <Container>
@@ -34,14 +44,138 @@ const OneToken = () => {
             </SpaceRow>
             
             <Spacer height="50px" />
+
+            <WhatsNewContainer>
+                <Line color="#AEDD00"/>
+
+                <div style={{margin: "40px"}}>
+                    <WhatsNewSubtitle color={Theme.colors.primary}>
+                        {t('What\'s new on')}&nbsp;<WhatsNewSubtitle color="#AEDD00">{t('SOY?')}</WhatsNewSubtitle>
+                    </WhatsNewSubtitle>
+                </div>
+
+                <CardsContainer>
+                    <Card img={WhatsNewXT} />
+                    <Card img={WhatsNewStaking}>
+                        <StyledCardTitle>{t("Soy Staking")}</StyledCardTitle>
+                        <StyledCardSubTitle>{t("Stake Soy, Earn SOY")}</StyledCardSubTitle>
+                        <CardBlob>
+                            <CardBlobAsset src={WhatsNewStaking} alt="" />
+                            <StyledCardText>{t("APR")}: {234.93}%</StyledCardText>
+                        </CardBlob>
+                    </Card>
+                    <Card img={WhatsNewFarming}>
+                        <StyledCardTitle>{t("Yield Farming")}</StyledCardTitle>
+                        <StyledCardSubTitle>{t("High APR Farms")}</StyledCardSubTitle>
+                        {["XMS - SOY", "ANTEX - SOY", "BCOIN - SOY"].map((name) => {
+                            return (
+                            <CardBlob>
+                                <CardBlobAsset src={WhatsNewStaking} alt="" />
+                                <StyledCardText style={{fontWeight: 600}}>+</StyledCardText>
+                                <CardBlobAsset src={WhatsNewStaking} alt="" />
+                                <StyledCardText>
+                                    <span style={{fontWeight: 600}}>{name}</span>
+                                    <br />{t("APR")}: {234.93}%</StyledCardText>
+                            </CardBlob>
+                            )
+                        })}
+                    </Card>
+                    <Card img={WhatsNewBurn}>
+                        <StyledCardTitle color={Theme.colors.white}>{t("Total Token Burn")}</StyledCardTitle>
+                        <StyledCardSubTitle color={Theme.colors.white} style={{fontWeight: 900}}>{burnedSoy} $SOY</StyledCardSubTitle>
+                    </Card>
+                </CardsContainer>
+            </WhatsNewContainer>
         </Container>
     )
 }
+
+/* WHAT's NEW */
+
+const WhatsNewContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    max-width: 1500px;
+    margin-bottom: 80px;
+`
+
+const CardsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+`
+
+const Card = styled.div<{img?: string}>`
+    width: 328px;
+    height: 400px;
+    padding: 20px;
+    margin: 10px;
+    background-image: ${({img}) => `url(${img})`};
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 20px;
+
+    @media screen and (max-width: 768px) {
+    }
+    
+    @media screen and (max-width: 768px) {
+    }
+`
+
+const CardBlob = styled.div`
+    width: auto;
+    border-radius: 20px;
+    background-color: rgba(255,255,255,0.7);
+    padding: 20px;
+    margin: 10px 0px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+const CardBlobAsset = styled.img`
+    width: 48px;
+    height: 48px;
+`
+
+const StyledCardText = styled.p<{color?: string}>`
+    color: ${({color}) => color || Theme.colors.black};
+    font-size: 16px;
+    font-family: ${Theme.fonts.text};
+    line-height: 24px;
+    white-space: nowrap;
+    letter-spacing: 5%;
+    @media screen and (max-width: 1200px) {
+    }
+    @media screen and (max-width: 768px) {
+        /* font-size: 44.2px;
+        line-height: 53.04px; */
+    }
+`;
+
+const StyledCardSubTitle = styled(StyledCardText)`
+    font-size: 20px;
+    line-height: 34px;
+    font-weight: 600;
+`
+
+const StyledCardTitle = styled(StyledCardText)`
+    font-size: 25px;
+    line-height: 38px;
+    font-weight: 700;
+`
+
+/* MAIN PAGE */
 
 const Container = styled.div`
     display: flex;
     position: relative;
     flex-direction: column;
+    align-items: center;
     width: 100%;
     padding: 0;
     background-color: #FFF;
@@ -94,6 +228,19 @@ const Title = styled.p<{color: string}>`
         // font-size: 18px;
     }
 `;
+const WhatsNewSubtitle = styled(Title)<{color: string}>`
+font-weight: 400;
+font-size: 60px;
+line-height: 88.2px;
+letter-spacing: 5%;
+@media screen and (max-width: 900px ) {
+    font-size: 38px;
+    line-height: 52px;
+}
+@media screen and (max-width: 768px ) {
+    // font-size: 18px;
+}
+`;
 const BkImage = styled.div`
     position: absolute;
     width: 100%;
@@ -141,4 +288,5 @@ const LeftAniImg = styled.img`
         width: 100px;
     }
 `
+
 export default OneToken;
