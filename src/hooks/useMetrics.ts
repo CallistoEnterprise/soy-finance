de53da7b8c07eb6461179934fd61d6412ec28696
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-07 12:56:01 +0100

    fix the deta api

--- src/hooks/useMetrics.ts
+++ src/hooks/useMetrics.ts
@@ -1,36 +1,76 @@
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
 import { useEffect, useState } from 'react'
 
-const METRICS = 'https://soy-finance.deta.dev/soy_metrics'
+const METRICS_API_ENDPOINT = 'https://soy-finance.deta.dev/soy_metrics'
+
+const METRICS_DEFAULT_ZEROES = {
+  runtime: 0.031,
+  block_number: 8392129,
+  result: {
+    Users: 0,
+    Trades: 0,
+    Volume: 0,
+    Volume_24h: 0,
+    Soy_Circulating_Supply: 0,
+    Soy_IDO: 0,
+    Total_Value_Locked_In_Farms: 0,
+  },
+}
+
+const METRICS_DEFAULT_MOCK = {
+  runtime: 0.031,
+  block_number: 8392129,
+  result: {
+    Users: 8930,
+    Trades: 4090000,
+    Volume: 89130000,
+    Volume_24h: 152610,
+    Soy_Circulating_Supply: 73380000,
+    Soy_IDO: 798570,
+    Total_Value_Locked_In_Farms: 2340000,
+  },
+}
+
+const CONFIG_USE_MOCK_DATA = true
+const CONFIG_API_TRIES = 10
 
 export const useMetrics = () => {
-  const [metricsData, setMetricsData] = useState({
-    runtime: 0.031,
-    block_number: 8392129,
-    result: {
-      Users: 0,
-      Trades: 0,
-      Volume: 0,
-      Volume_24h: 0,
-      Soy_Circulating_Supply: 0,
-      Soy_IDO: 0,
-      Total_Value_Locked_In_Farms: 0,
-    },
-  })
+  const [metricsData, setMetricsData] = useState(CONFIG_USE_MOCK_DATA ? METRICS_DEFAULT_MOCK : METRICS_DEFAULT_ZEROES)
 
   useEffect(() => {
     const fetchData = async () => {
-      axios
-        .get(METRICS)
-        .then((response) => {
-          if (response.data) {
-            setMetricsData(response.data)
-          }
-        })
-        .catch((err) => {
+      let errorEncountered = false // api errored?
+      let counter = 0 // times loop finished
+      let response: AxiosResponse<any>
+
+      while(!errorEncountered || response === undefined) {
+        errorEncountered = false;
+
+        try { // eslint-disable-next-line
+          response = await axios.get(METRICS_API_ENDPOINT)
+        } catch(err) {
           console.error(err)
-        })
+          errorEncountered = true
+        }
+
+        counter += 1
+        if(counter === CONFIG_API_TRIES)
+          break;
+      }
+
+      setMetricsData(response.data)
+        /* axios
+          .get(METRICS_API_ENDPOINT)
+          .then((response) => {
+            if (response.data) {
+              setMetricsData(response.data)
+            }
+          })
+          .catch((err) => {
+            console.error(err)
+          }) */
     }
+
     fetchData()
   }, [])
 
