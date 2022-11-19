aa9ea82b35cf8240972b88b8e5a4928d5cfc9ac0
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-19 12:42:01 +0100

    some more libs removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { ResetCSS } from '@callisto-enterprise/soy-uikit2'
-import App from 'App'
+import App from './App'
 import GlobalStyle from './style/Global'
 import Providers from './Providers'
 import 'bootstrap/dist/css/bootstrap.min.css'
