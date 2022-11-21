3a74c7171a7d429e452e2a34a0fabc4762d00b48
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-21 10:13:19 +0100

    redux removal unused

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,22 +1,18 @@
 import { LanguageProvider } from 'contexts/Localization'
 import React from 'react'
-import { Provider } from 'react-redux'
 import { Web3ReactProvider } from '@web3-react/core'
 import { ModalProvider } from 'styled-react-modal'
 import { getLibrary } from './hooks/getLibrary'
-import store from './store'
 import { ThemeContextProvider } from './ThemeContext'
 
 const Providers: React.FC = ({ children }) => {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Provider store={store}>
-        <ThemeContextProvider>
-          <LanguageProvider>
-            <ModalProvider>{children}</ModalProvider>
-          </LanguageProvider>
-        </ThemeContextProvider>
-      </Provider>
+      <ThemeContextProvider>
+        <LanguageProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </LanguageProvider>
+      </ThemeContextProvider>
     </Web3ReactProvider>
   )
 }
--- src/rootSaga.ts
+++
@@ -1,6 +0,0 @@
-import { all } from 'redux-saga/effects'
-import HomeSaga from './state/home/saga'
-
-export default function* rootSaga() {
-  yield all([HomeSaga()])
-}
