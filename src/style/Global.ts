4bc3a28937db286a9850b5242500ee6813598bdc
Rachid Lajmi <mosaixel.org@gmail.com>
2023-04-07 14:19:17 +0100

    white_bg

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -2,11 +2,14 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
     body {
-        background-color: #b0d400;
+        
         img {
             height: auto;
             max-width: 100%;
         }
+    }
+    nav{
+        background-color: #b0d400;
     }
 `
 export default GlobalStyle
